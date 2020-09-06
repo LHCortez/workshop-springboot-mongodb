6d56b88f2d7b7d6733800e7788a4a78abda400b1
LHCortez <luizcquintas@gmail.com>
2020-09-06 14:37:07 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/luizcortez/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizcortez.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
